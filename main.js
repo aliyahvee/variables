15c17e1a4e68e662e138ce5b1bba735645a1b72b
Aliyah Veal <Aliyah@Aliyahs-MBP-2.home>
2018-03-13 11:55:40 -0400

    variables added

---
+++ main.js
@@ -0,0 +1,10 @@
+var grant = 1000;
+var funding = {
+  ngos: ["Help Unlimited", "New Start", "Better Tomorrow", "Home Safe", "New Home"]
+  monthlyexpenses: [800, 950, 1000, 1100, 1200]
+}
+var single = 1
+var couple = 2
+var family = {
+  number: [3, 4, 5]
+};
